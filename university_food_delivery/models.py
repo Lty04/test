d8a27031277cabbac9dad9ff2e425cfc6233ff03
Lty04 <140261358+Lty04@users.noreply.github.com>
2026-04-20 22:28:11 +0800

    Update models.py

--- university_food_delivery/models.py
+++ university_food_delivery/models.py
@@ -25,10 +25,10 @@ class User(UserMixin, db.Model):
     reviews = db.relationship('Review', backref='user', lazy=True)
     
     def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
 
 
 class Restaurant(db.Model):
